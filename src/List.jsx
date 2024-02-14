e4c98abd30a30948bea19a2e5944f63c917aed48
Peter <gudqo1995@naver.com>
2024-02-15 00:21:26 +0900

    header radio

--- src/List.jsx
+++ src/List.jsx
@@ -3,40 +3,102 @@ import { useEffect, useState } from "react";
 export default function List() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  const [display, setDisplay] = useState("ALL");
   const onChange = (e) => {
     setTodo(e.target.value);
   };
   const onClick = () => {
-    setTodos((prev) => [...prev, todo]);
+    setTodos((prev) => [...prev, { name: todo, checked: false }]);
     setTodo("");
   };
   const onDelete = (idx) => {
-    setTodos((prev) => prev.splice(idx, 1));
+    setTodos((prev) => prev.filter((item, index) => index !== idx));
+  };
+  const onChangeRadio = (e) => {
+    setDisplay(e.target.value);
+    console.log(e.target.value);
+    console.log(display);
   };
   return (
     <div>
       <div>
-        <input type="radio" name="header" id="all" />
+        <input
+          type="radio"
+          name="header"
+          id="all"
+          value="ALL"
+          onChange={onChangeRadio}
+          checked={display === "ALL"}
+        />
         <label htmlFor="all">All</label>
-        <input type="radio" name="header" id="active" />
+        <input
+          type="radio"
+          name="header"
+          id="active"
+          value="ACTIVE"
+          onChange={onChangeRadio}
+          checked={display === "ACTIVE"}
+        />
         <label htmlFor="active">Active</label>
-        <input type="radio" name="header" id="completed" />
+        <input
+          type="radio"
+          name="header"
+          id="completed"
+          value="COMPLETED"
+          onChange={onChangeRadio}
+          checked={display === "ACTIVE"}
+        />
         <label htmlFor="completed">Completed</label>
       </div>
       <ul>
-        {todos.map((todo, idx) => (
-          <div style={{ display: "flex" }}>
-            <li key={idx}>
-              <input type="checkbox" /> {todo}
-            </li>
-            <span
-              style={{ display: "flex", paddingLeft: "10px" }}
-              onClick={() => onDelete({ idx })}
-            >
+        {/* {todos.map((oneTodo, idx) => (
+          <li key={idx} style={{ display: "flex" }}>
+            <input type="checkbox" /> {oneTodo.name}
+            <span style={{ paddingLeft: "10px" }} onClick={() => onDelete(idx)}>
               X
             </span>
-          </div>
-        ))}
+          </li>
+        ))} */}
+        {display === "ALL" &&
+          todos.map((oneTodo, idx) => (
+            <li key={idx} style={{ display: "flex" }}>
+              <input type="checkbox" /> {oneTodo.name}
+              <span
+                style={{ paddingLeft: "10px" }}
+                onClick={() => onDelete(idx)}
+              >
+                X
+              </span>
+            </li>
+          ))}
+        {display === "ACTIVE" &&
+          todos
+            .filter((item) => !item.checked)
+            .map((oneTodo, idx) => (
+              <li key={idx} style={{ display: "flex" }}>
+                <input type="checkbox" /> {oneTodo.name}
+                <span
+                  style={{ paddingLeft: "10px" }}
+                  onClick={() => onDelete(idx)}
+                >
+                  X
+                </span>
+              </li>
+            ))}
+        {display === "COMPLETED" &&
+          todos
+            .filter((item) => item.checked)
+            .map((oneTodo, idx) => (
+              <li key={idx} style={{ display: "flex" }}>
+                <input type="checkbox" /> {oneTodo.name}
+                <span
+                  style={{ paddingLeft: "10px" }}
+                  onClick={() => onDelete(idx)}
+                >
+                  X
+                </span>
+              </li>
+            ))}
       </ul>
       <input type="text" value={todo} onChange={onChange} />
       <button onClick={onClick}>Add</button>
