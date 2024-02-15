e36dccffffe1e1bad3f6f6fd020d562ac3bd6bce
Peter <gudqo1995@naver.com>
2024-02-16 00:54:53 +0900

    todo filtering

--- src/List.jsx
+++ src/List.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
-
+/**
+ * 컴포넌트화 해보기
+ * 다크모드 지원
+ * 로컬스토리지에 저장
+ * @returns
+ */
 export default function List() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
@@ -16,8 +21,18 @@ export default function List() {
   };
   const onChangeRadio = (e) => {
     setDisplay(e.target.value);
-    console.log(e.target.value);
-    console.log(display);
+  };
+  const onChangeCheckbox = (idx) => {
+    setTodos((prev) =>
+      prev.map((item, index) => {
+        if (index === idx)
+          return {
+            ...item,
+            checked: !item.checked,
+          };
+        return item;
+      })
+    );
   };
   return (
     <div>
@@ -46,59 +61,29 @@ export default function List() {
           id="completed"
           value="COMPLETED"
           onChange={onChangeRadio}
-          checked={display === "ACTIVE"}
+          checked={display === "COMPLETED"}
         />
         <label htmlFor="completed">Completed</label>
       </div>
       <ul>
-        {/* {todos.map((oneTodo, idx) => (
+        {(display === "ALL"
+          ? todos
+          : todos.filter((item) =>
+              display === "COMPLETED" ? item.checked : !item.checked
+            )
+        ).map((oneTodo, idx) => (
           <li key={idx} style={{ display: "flex" }}>
-            <input type="checkbox" /> {oneTodo.name}
+            <input
+              type="checkbox"
+              checked={oneTodo.checked}
+              onChange={() => onChangeCheckbox(idx)}
+            />{" "}
+            {oneTodo.name}
             <span style={{ paddingLeft: "10px" }} onClick={() => onDelete(idx)}>
               X
             </span>
           </li>
-        ))} */}
-        {display === "ALL" &&
-          todos.map((oneTodo, idx) => (
-            <li key={idx} style={{ display: "flex" }}>
-              <input type="checkbox" /> {oneTodo.name}
-              <span
-                style={{ paddingLeft: "10px" }}
-                onClick={() => onDelete(idx)}
-              >
-                X
-              </span>
-            </li>
-          ))}
-        {display === "ACTIVE" &&
-          todos
-            .filter((item) => !item.checked)
-            .map((oneTodo, idx) => (
-              <li key={idx} style={{ display: "flex" }}>
-                <input type="checkbox" /> {oneTodo.name}
-                <span
-                  style={{ paddingLeft: "10px" }}
-                  onClick={() => onDelete(idx)}
-                >
-                  X
-                </span>
-              </li>
-            ))}
-        {display === "COMPLETED" &&
-          todos
-            .filter((item) => item.checked)
-            .map((oneTodo, idx) => (
-              <li key={idx} style={{ display: "flex" }}>
-                <input type="checkbox" /> {oneTodo.name}
-                <span
-                  style={{ paddingLeft: "10px" }}
-                  onClick={() => onDelete(idx)}
-                >
-                  X
-                </span>
-              </li>
-            ))}
+        ))}
       </ul>
       <input type="text" value={todo} onChange={onChange} />
       <button onClick={onClick}>Add</button>
